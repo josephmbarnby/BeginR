ce926ff72f9435f5a826bac8cff80d9e07413019
Joseph M Barnby <josephmbarnby@gmail.com>
2022-08-10 21:43:13 +0800

    Add files via upload

--- app.R
+++ app.R
@@ -4,6 +4,7 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(tidyr)
+library(patchwork)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -18,12 +19,16 @@ ui <- fluidPage(
                   Over time the agent will come to learn which card will give it most reward; the
                   agent will seek to maximise its return.",
                   "<br/>",
-                  "The rate at which the agent will explore its environment is governed by two parameters:
+                  "The way in which the agent acts upon the environment to maximise its return is to convert its beliefs into actions",
+                  "<br/>",
+                  "<i><center> Beliefs </i> &#8594; <i> Actions </i></center>",
+                  "<br/>",
+                  "The rate at which the agent will explore its environment, generate new beliefs, and make optimised actions is governed by two parameters:
                   a learning rate (how quickly an agent learns from each reward) and a decision temperature (how noisily an agent chooses
                   between each option)",sep="<br/>")))),
 
     titlePanel(h4("This agent uses the following equations:")),
-
+    br(),
     withMathJax(),
       tabPanel(
       title = "Diagnostics",
@@ -51,12 +56,14 @@ ui <- fluidPage(
                         max = 1,
                         value = 0.1,
                         step = 0.01),
+            br(),
             sliderInput("tau",
                         HTML(paste("Decision Temperature: (&tau;)")),
                         min = 0.1,
                         max = 10,
                         value = 1,
                         step = 0.1),
+            br(),
            # sliderInput("gamma",
            #             HTML(paste("Discount Rate: (&gamma;)")),
            #             min = 0,
@@ -69,21 +76,24 @@ ui <- fluidPage(
                         max = 1000,
                         value = 500,
                         step = 50),
+            br(),
             sliderInput("winprob",
                         "Win Probability of Card 2:",
                         min = 0,
                         max = 1,
                         value = 0.8,
-                        step = 0.1)
+                        step = 0.1),
+           br()#,
+           #actionButton("colour", "I'm bored of these colours")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot", height = '500px', width = 'auto')
+           plotOutput("distPlot", height = '700px', width = 'auto')
         )
     ),
 
-    titlePanel(h6("CC Dr Joe M Barnby 2022"))
+    titlePanel(h5(HTML(paste("CC <a href='https://www.joebarnby.com/'>Dr Joe M Barnby</a> 2022"))))
 
 )
 
@@ -112,39 +122,48 @@ server <- function(input, output) {
 
         Q2[1,]  <- c(0.5, 0.5) # Initialize the first two actions as equal probabilities
         R2      <- matrix(c(1-input$winprob, input$winprob, input$winprob, 1-input$winprob), 2, 2)
-        r       <- prob_a1 <- rep(NA, trials+1)
+        a       <- r <- prob_a1 <- rep(NA, trials+1)
 
         #Sample the cards
-          observeEvent(input$setseed, {
+        observeEvent(input$setseed, {
           set.seed(sample(1:100000, 1))
         })
+
+        #Set colours
+        defcols <- c("#E41A1C" ,"#377EB8")
+        #observeEvent(input$colour, {
+        #defcols <- sample(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00",
+        #                      "#FFFF33", "#A65628", "#F781BF"), 2, replace = F)
+        #})
+
         for (t in 1:trials){
 
           #sample an action
           a1         <- exp(Q2[t,1]/tau)
           a2         <- exp(Q2[t,2]/tau)
           prob_a1[t] <- a1/(a1+a2)
-          a          <- sample(c(1,2),  1, T, prob = c(prob_a1[t], 1-prob_a1[t]))
+          a[t]       <- sample(c(1,2),  1, T, prob = c(prob_a1[t], 1-prob_a1[t]))
 
           #sample a reward for the action
-          prob_r     <- R2[a,]
+          prob_r     <- R2[a[t],]
           r[t]       <- sample(c(1, 0), 1, T, prob = prob_r)
 
           #update
-          PE         <- r[t] - Q2[t, a]
+          PE         <- r[t] - Q2[t, a[t]]
 
           #if(input$gamma > 0){
-          #Q2[t+1, a] <- Q2[t, a] + lambda * (gamma * r + (max(Q2[t,]) - Q2[t, a])) #RW equation w discount
+          #Q2[t+1, a[t]] <- Q2[t, a[t]] + lambda * (gamma * r + (max(Q2[t,]) - Q2[t, a[t]])) #RW equation w discount
           #} else{
-          Q2[t+1, a] <- Q2[t, a] + (lambda * PE) #RW equation
+          Q2[t+1, a[t]] <- Q2[t, a[t]] + (lambda * PE) #RW equation
           #}
-          Q2[t+1,-a] <- Q2[t,-a]
+          Q2[t+1,-a[t]] <- Q2[t,-a[t]]
 
         }
 
         # draw the choice selection
+
         colnames(Q2) <- c('Card 1', 'Card 2')
-        Q2 %>%
+        mainplot <- Q2 %>%
           as.data.frame() %>%
           mutate(Trial = 0:trials,
                  ProbA1= prob_a1) %>%
@@ -153,15 +172,15 @@ server <- function(input, output) {
           ggplot(aes(Trial, Q, color = Option))+
           geom_line()+
           geom_line(aes(Trial, ProbA1), linetype = 2, color = 'black')+
-          geom_text(x = trials*0.25, y = 0.05,
-                    label = paste('Total rewards received = ',sum(r[1:trials])),
-                    color = 'black', check_overlap = T, nudge_y = 0.1, fontface = 'bold', size = 5)+
+          #geom_text(x = trials*0.25, y = 0.05,
+          #          label = paste('Total rewards received = ',sum(r[1:trials])),
+          #          color = 'black', check_overlap = T, nudge_y = 0.1, fontface = 'bold', size = 5)+
           geom_hline(yintercept = c(input$winprob, 1-input$winprob),
-                     color = c("#377EB8", "#E41A1C"),
+                     color = defcols,
                      linetype = 2, alpha = 0.2)+
           coord_cartesian(ylim = c(0,1))+
           scale_color_brewer(palette = 'Set1')+
-          labs(y = expression(paste('Q'[c]^t)))+
+          labs(y = expression(paste('Q'[c]^t, '    &    p(' ,hat(c), '= c)')))+
           scale_y_continuous(breaks = seq(0, 1, 0.2), labels = seq(0, 1, 0.2))+
           theme_bw() +
           theme(text = element_text(size = 20),
@@ -171,6 +190,29 @@ server <- function(input, output) {
                 legend.text = element_text(size = 20),
                 legend.position = c(0.9, 0.1),
                 legend.background = element_rect(color = 'black'))
+
+        subplot <- data.frame(Action = a, Reward = r) %>%
+          na.omit() %>%
+          mutate(Action1  = ifelse(Action == 1, 1, 0),
+                 Action2  = ifelse(Action == 2, 1, 0),
+                 ActionS1 = sum(Action1),
+                 ActionS2 = sum(Action2),
+                 RewardS  = sum(Reward)) %>%
+          dplyr::select(ActionS1, ActionS2, RewardS) %>%
+          rename(`Card 1 Choices` = 1, `Card 2 Choices` = 2, `Rewards`= 3) %>%
+          distinct() %>%
+          pivot_longer(1:3, 'Index', values_to = 'Value') %>%
+          ggplot(aes(Index, Value))+
+          geom_col(fill = c(defcols, "#FFB302"), color = 'black')+
+          coord_cartesian(ylim = c(0, trials))+
+          labs(title = 'Sum of...')+
+          theme_bw() +
+          theme(text = element_text(size = 20),
+                plot.title = element_text(face = 'bold', vjust = -6, hjust = 0.05),
+                axis.title = element_blank(),
+                axis.text = element_text(size = 20))
+#
+        (mainplot / subplot) & plot_layout(nrow = 2, heights = c(2,1))
     })
 }
 
